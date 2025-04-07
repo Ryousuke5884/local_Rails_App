58198fc6f0176d92ba9de1b0a2da92da3938d91c
小林諒佑\Ryous <m0a221086b@edu.teu.ac.jp>
2025-04-07 16:27:51 +0900

    finish the 7 except 7.5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    #下の関数でStrongParamatorを使って制限した値でnewをする
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+
+      redirect_to user_url(@user)
+    else
+      render "new",status: :unprocessable_entity
+    end
+    
+  end
+
+
+  #strongParametorを使ってユーザーが送信できる情報に制限をかける、admin属性などをいじられないようにするため
+  private
+   def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+   end
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,45 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  #無効なユーザー登録を行った場合のテスト
+
+
+  test "invalid signup information" do
+    get signup_path
+
+    #このassert_no_differenceと引数のUser.countは、失敗するはずの内容でuserをpostした前後でUserの数が変わらないことを確認している
+    #User.countに""がついている理由は、中身を実行した後に呼ぶため、ちなみにアロー関数にすれば別に""付けなくて大丈夫
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: "",
+      email: "user@invalid",
+      password: "foo",
+      password_confirmation: "bar" } }
+    end
+
+    #送信に失敗したら、HTMLコード422で更新してかつusers/newに戻っている
+    assert_response :unprocessable_entity
+    assert_template "users/new"
+
+    assert_select "div#error_explanation"
+    assert_select "div.field_with_errors"
+  end
+  
+  test "valid signup information" do
+    #differenceでuser.countの数が１増えるのを確認
+
+    assert_difference "User.count" , 1 do
+      post users_path, params: { user: { name: "Example User",
+                                        email: "user@example.com",
+                                        password: "password",
+                                        password_confirmation: "password" } }
+    end
+
+    follow_redirect!
+    assert_template "users/show"
+
+    assert_not flash.empty?
+  end
+
+end
+
+
